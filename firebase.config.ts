44b83575f600807ee19bab532227aa6d3a077def
elisio42 <elisiouiux@gmail.com>
2025-02-05 14:56:41 +0100

    deixar de fora o arquivo .env.local

--- firebase.config.ts
+++ firebase.config.ts
@@ -1,20 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAR_Da2hEh-uW--2PD9mC4i-_u60-TEMk0",
-  authDomain: "care-5e2d6.firebaseapp.com",
-  projectId: "care-5e2d6",
-  storageBucket: "care-5e2d6.firebasestorage.app",
-  messagingSenderId: "912201237977",
-  appId: "1:912201237977:web:eaf1257351b6b930d145c8"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+
 export { auth };
