0ab741ce8d4fe75da902c3bc7d48b71e5bc32f50
elisio42 <elisiouiux@gmail.com>
2025-02-28 11:26:45 +0100

    header done

--- src/components/UserDropdown.tsx
+++ src/components/UserDropdown.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { DropdownItem } from "../ui/dropdown/DropdownItem";
-import { Dropdown } from "../ui/dropdown/Dropdown";
+import { DropdownItem } from "./ui/dropdown/DropdownItem";
+import { Dropdown } from "./ui/dropdown/Dropdown";
 import { Link } from "react-router";
 
 export default function UserDropdown() {
@@ -23,7 +23,7 @@ export default function UserDropdown() {
           <img src="/images/user/owner.jpg" alt="User" />
         </span>
 
-        <span className="block mr-1 font-medium text-theme-sm">Musharof</span>
+        <span className="block mr-1 font-medium text-theme-sm">Mendes</span>
         <svg
           className={`stroke-gray-500 dark:stroke-gray-400 transition-transform duration-200 ${
             isOpen ? "rotate-180" : ""
@@ -51,10 +51,10 @@ export default function UserDropdown() {
       >
         <div>
           <span className="block font-medium text-gray-700 text-theme-sm dark:text-gray-400">
-            Musharof Chowdhury
+            Mendes Correia
           </span>
           <span className="mt-0.5 block text-theme-xs text-gray-500 dark:text-gray-400">
-            randomuser@pimjo.com
+            mendes@gmail.com
           </span>
         </div>
 
@@ -81,7 +81,7 @@ export default function UserDropdown() {
                   fill=""
                 />
               </svg>
-              Edit profile
+              Editar perfil
             </DropdownItem>
           </li>
           <li>
@@ -106,7 +106,7 @@ export default function UserDropdown() {
                   fill=""
                 />
               </svg>
-              Account settings
+              Definicoes conta
             </DropdownItem>
           </li>
           <li>
@@ -131,7 +131,7 @@ export default function UserDropdown() {
                   fill=""
                 />
               </svg>
-              Support
+              Suporte
             </DropdownItem>
           </li>
         </ul>
@@ -154,7 +154,7 @@ export default function UserDropdown() {
               fill=""
             />
           </svg>
-          Sign out
+          Sair
         </Link>
       </Dropdown>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
+import { SidebarProvider } from "./context/SidebarContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <SidebarProvider>
+        <App />
+      </SidebarProvider>
     </ThemeProvider>
   </StrictMode>
 );
