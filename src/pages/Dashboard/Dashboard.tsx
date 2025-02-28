5b0c37a217b391e7db235cebb1a95d88f4c14dfc
elisio42 <elisiouiux@gmail.com>
2025-02-28 13:55:48 +0100

    header done

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,12 +1,11 @@
 import AppHeader from "../../layout/AppHeader";
 import AppSidebar from "../../layout/AppSidebar";
 
-
 export default function Dashboard() {
   return (
     <div>
-       <AppHeader />
-    
+      <AppHeader />
+      <AppSidebar />
     </div>
-  )
+  );
 }
