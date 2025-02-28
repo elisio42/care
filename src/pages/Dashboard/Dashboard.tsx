8d030614b4073cba6fcf6c93f2bb928a60ecd63d
elisio42 <elisiouiux@gmail.com>
2025-02-28 17:42:29 +0100

    fixheader error

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,11 +1,15 @@
+import { useTheme } from "../../context/ThemeContext";
 import AppHeader from "../../layout/AppHeader";
 import AppSidebar from "../../layout/AppSidebar";
 
 export default function Dashboard() {
+  const { theme } = useTheme();
+
   return (
-    <div>
+    <div className={`w-full h-full ${theme === "dark" ? "#fff" : "#101828"}`}>
       <AppHeader />
       <AppSidebar />
+      
     </div>
   );
 }
