766708796681cc117689e3f6aefd1b7e6f99a4bc
elisio42 <elisiouiux@gmail.com>
2025-02-28 12:12:31 +0100

    make sidebar

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,12 @@
 import AppHeader from "../../layout/AppHeader";
+import AppSidebar from "../../layout/AppSidebar";
 
 
 export default function Dashboard() {
   return (
     <div>
        <AppHeader />
+    
     </div>
   )
 }
